dbe49c58c84e51743a3183e7069a4c82f0ef6585
BraunD <d.braun@eos-ts.com>
2019-04-12 17:51:06 +0200

    initial commit

---
+++ src/main/java/de/qacrobat/springdemogradle/SpringdemogradleApplication.java
@@ -0,0 +1,13 @@
+package de.qacrobat.springdemogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemogradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemogradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/qacrobat/springdemogradle/SpringdemogradleApplicationTests.java
@@ -0,0 +1,16 @@
+package de.qacrobat.springdemogradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringdemogradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
