e5b3daa6516253d15950e3cc13c96b2331078fb5
David Braun <d.braun@eos-ts.com>
2019-07-15 16:35:29 +0200

    add tests for PlayerRepositoryImpl

--- src/test/java/de/qacrobat/springdemogradle/player/PlayerRepositoryImplTest.java
+++ src/test/java/de/qacrobat/springdemogradle/player/PlayerRepositoryImplTest.java
@@ -1,10 +1,51 @@
 package de.qacrobat.springdemogradle.player;
 
+import org.junit.Before;
+import org.junit.Test;
 
-import com.tngtech.jgiven.testng.ScenarioTest;
-import de.qacrobat.springdemogradle.player.steps.GivenPlayer;
-import de.qacrobat.springdemogradle.player.steps.ThenPlayer;
-import de.qacrobat.springdemogradle.player.steps.WhenPlayer;
+import java.util.Optional;
 
-public class PlayerRepositoryImplTest extends ScenarioTest<GivenPlayer, WhenPlayer, ThenPlayer> {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class PlayerRepositoryImplTest {
+
+    protected static final long PLAYER_ID = 17L;
+
+    private PlayerRepositoryImpl testee;
+    private Player playerMock;
+
+    @Before
+    public void setUp() {
+        // given
+        testee = new PlayerRepositoryImpl();
+        playerMock = mock(Player.class);
+        when(playerMock.getId()).thenReturn(PLAYER_ID);
+    }
+
+    @Test
+    public void shouldSaveNewSchuldner() {
+        // when
+        testee.saveOrUpdate(playerMock);
+
+        // then
+        assertThat(testee.findById(PLAYER_ID).get()).isEqualTo(playerMock);
+    }
+
+    @Test
+    public void shouldDeleteSchuldner() {
+        // given
+        testee.saveOrUpdate(playerMock);
+
+        // when
+        testee.deletePlayer(PLAYER_ID);
+
+        // then
+        final Optional<Player> result = testee.findById(PLAYER_ID);
+        assertThat(result.isPresent()).isFalse();
+
+    }
+
 }
